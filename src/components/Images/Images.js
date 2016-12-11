cf97769a009a2d53577d283c3d288ebf55b8e734
Espen Meidell <espen.meidell@gmail.com>
2016-12-11 17:48:07 +0100

    Loading some stuff works

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -6,19 +6,26 @@ class Images extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      nexturl: 'https://api.instagram.com/v1/users/3017821590/media/recent?access_token=3017821590.1677ed0.21c3cbc98fc1494e822ae09f6009a3ac&count=24',
+      images: []
+    }
   }
 
 
 
   componentWillMount() {
     $.ajax({
-      url: 'https://api.instagram.com/v1/users/3017821590/media/recent?access_token=3017821590.1677ed0.21c3cbc98fc1494e822ae09f6009a3ac',
+      url: 'https://api.instagram.com/v1/users/3017821590/media/recent?access_token=3017821590.1677ed0.21c3cbc98fc1494e822ae09f6009a3ac&count=24',
       type: "GET",
       crossDomain: true,
       dataType: "jsonp",
       success: (data) => {
-        console.log(data.data)
+        var newImages = this.state.images.concat(data.data)
+        this.setState({
+          nexturl: data.pagination.next_url,
+          images: newImages
+        })
       }
     })
 
@@ -26,16 +33,16 @@ class Images extends Component {
 
 
   render() {
+    var images = []
+    for (var i = 0; i < this.state.images.length; i++) {
+      images.push(<Image key={i} data={this.state.images[i]} />)
+    }
+
     return (
       <div className="images">
         <h2>Images</h2>
         <div className="image-grid">
-          <Image data="Yo" />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
-          <Image />
+          {images}
         </div>
       </div>
     );
