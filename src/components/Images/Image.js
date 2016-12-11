4bb097dd94b3df73b9d1d65a5285aea5fb961517
Espen Meidell <espen.meidell@gmail.com>
2016-12-11 18:07:39 +0100

    Smud moment

--- src/components/Images/Image.js
+++ src/components/Images/Image.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import moment from 'moment'
 
 class Images extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {data: props.data}
+    this.state = {
+      data: props.data,
+      url: 'http://placehold.it/320x320',
+      bigurl: 'http://placehold.it/640x640',
+      time: 0
+    }
   }
 
 
 
   componentWillMount() {
-    console.log(this.state.data)
+    moment.lang('nb')
+    this.setState({
+      url:this.state.data.images.low_resolution.url,
+      bigurl: this.state.data.images.standard_resolution.url,
+      time: this.state.data.created_time
+    })
   }
 
 
   render() {
+
     return (
       <div className="image">
-        <img src="http://placehold.it/320x320"/>
-        <p className="time">This is an image.</p>
+        <img src={this.state.url}/>
+        <p className="time">{moment.unix(this.state.time).fromNow()}</p>
       </div>
     );
   }
