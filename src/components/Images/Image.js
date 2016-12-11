177f0d22e5602317f9923e6098e4e5b3aef8df4e
Espen Meidell <espen.meidell@gmail.com>
2016-12-11 17:08:24 +0100

    Sliter med cors

--- src/components/Images/Image.js
+++ src/components/Images/Image.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class Images extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {data: props.data}
+  }
+
+
+
+  componentWillMount() {
+    console.log(this.state.data)
+  }
+
+
   render() {
     return (
       <div className="image">
         <img src="http://placehold.it/320x320"/>
-        <h3>This is an image.</h3>
+        <p className="time">This is an image.</p>
       </div>
     );
   }
--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react';
 import Image from './Image'
 
 class Images extends Component {
+
+  constructor() {
+    super()
+
+  }
+
+  componentWillMount() {
+    const url = 'https://api.instagram.com/v1/users/3017821590/media/recent?access_token=3017821590.1677ed0.21c3cbc98fc1494e822ae09f6009a3ac'
+    var headers = { method: 'GET',
+               mode: 'no-cors',
+               cache: 'default'}
+    fetch(url, headers)
+      .then((response) => {
+        console.log("got response")
+        console.log(JSON.parse(response))
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }
+
+
   render() {
     return (
       <div className="images">
         <h2>Images</h2>
         <div className="image-grid">
-          <Image />
+          <Image data="Yo" />
           <Image />
           <Image />
           <Image />
