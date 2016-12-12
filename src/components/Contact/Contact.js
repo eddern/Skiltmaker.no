3a088acabf5851371a6bcb8da2705aa6223e12e5
Espen Meidell <espen.meidell@gmail.com>
2016-12-12 16:11:20 +0100

    Contact done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,12 +4,27 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <h1>Ta kontakt!</h1>
-        <p className="contact_info">
-          <span className="glyphicon glyphicon-earphone"></span> 11 22 33 44
-          <br/>
-          <span className="glyphicon glyphicon-envelope"></span> <a href="mailto:post@skiltmaker.no">post@skiltmaker.no</a>
-        </p>
+        <table>
+          <tr>
+            <td colSpan="2"><h1>Kontaktinformasjon</h1></td>
+          </tr>
+          <tr>
+            <td><i className="fa fa-lg fa-mobile" aria-hidden="true"></i></td><td><a href="tel:+4791158975">+47 911 58 975</a></td>
+          </tr>
+          <tr>
+            <td><i className="fa fa-phone" aria-hidden="true"></i></td><td><a href="tel:+4722490606">+47 22 49 06 06</a></td>
+          </tr>
+          <tr>
+            <td><i className="fa fa-fax" aria-hidden="true"></i></td><td><a href="fax:+4722920602">+47 22 92 06 02</a></td>
+          </tr>
+          <tr>
+            <td><i className="fa fa-at" aria-hidden="true"></i></td><td><a href="mailto:post@skiltmaker.no">post@skiltmaker.no</a></td>
+          </tr>
+          <tr>
+            <td><i className="fa fa-envelope-o" aria-hidden="true"></i></td><td>Arnebråtveien 34<br/>0771 Oslo</td>
+          </tr>
+        </table>
+
       </div>
     );
   }
