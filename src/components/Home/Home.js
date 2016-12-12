b021cfd6b867ce5830df6d1f7c390198d8230923
eddern <edvardsj@stud.ntnu.no>
2016-12-12 10:45:03 +0100

    image carousel added for frontpage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ImageCarousel from './ImageCarousel'
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
         <h2>Home</h2>
+        <ImageCarousel />
+
       </div>
     );
   }
 }
 
-export default Home;
+export default Home
---
+++ src/components/Home/ImageCarousel.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import Carousel from 'nuka-carousel'
+
+import bil from './slideshow/bil.jpg'
+import cutout from './slideshow/cutout.jpg'
+import flagg from './slideshow/flagg.jpg'
+import folie from './slideshow/folie.jpg'
+import liteskilt from './slideshow/liteskilt.jpg'
+import lys from './slideshow/lys.jpg'
+
+
+class ImageCarousel extends Component {
+  mixins: [Carousel.ControllerMixin]
+  render(){
+
+    var Decorators = [{
+      component: React.createClass({
+        render() {
+          return (
+            <button className="carousel-btn" onClick={this.props.previousSlide}><i className="fa fa-angle-left" aria-hidden="true"></i></button>
+          )
+        }
+      }),
+      position: 'CenterLeft',
+      style: {
+        color: "#FFF",
+        fontSize: "200%"
+      }
+    },
+    {
+      component: React.createClass({
+        render() {
+          return (
+            <button className="carousel-btn" onClick={this.props.nextSlide}><i className="fa fa-angle-right" aria-hidden="true"></i></button>
+          )
+        }
+      }),
+      position: 'CenterRight',
+      style: {
+
+        color: "#FFF",
+        fontSize: "200%"
+      }
+    }];
+
+    return (
+      <Carousel
+        autoplay={true}
+        initialSlideHeight={640}
+        slidesToShow={3}
+        wrapAround={true}
+        cellSpacing={20}
+        autoplayInterval={2500}
+        decorators={Decorators}>
+        <img src={bil} alt="bil dekor" />
+        <img src={liteskilt} alt="småskilt" />
+        <img src={cutout} alt="utkuttet skilt" />
+        <img src={folie} alt="heldekkende folie" />
+        <img src={lys} alt="skilt med lys" />
+        <img src={flagg} alt="flagg og bannere" />
+      </Carousel>
+    )
+  }
+}
+export default ImageCarousel
