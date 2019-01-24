7dda1314ee2827c544db836bfb99c188dd609943
Edvard Schreiner Sjøblom <kajasolb@gmail.com>
2019-01-24 13:42:18 +0100

    updated footer links and made images out of different sizes fit the image box

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component{
         <div className="footer-content">
           <div className="copyright-text"><i className="fa fa-copyright" aria-hidden="true"></i> Skiltmaker Sjøblom, alle rettigheter reservert.</div>
           <div>
-            Nettside laget av <a href="https://www.espenmeidell.no">Espen</a> og <a href="https://www.eddern.no">Edvard</a>.
+            Nettside laget av <a href="https://www.linkedin.com/in/espen-meidell/">Espen</a> og <a href="https://www.linkedin.com/in/edvardsj/">Edvard</a>.
              <a href="https://github.com/eddern/Skiltmaker.no"><i className="fa fa-github" aria-hidden="true"></i></a></div>
         </div>
       </footer>
